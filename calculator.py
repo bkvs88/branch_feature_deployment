e84da3428bcd9b54baf3890b3c6c589ed3667e68
Sravan Kumar Bandaru <bkvs88@gmail.com>
2026-09-25 01:49:58 +0800

    Initial changes for branch testing

---
+++ calculator.py
@@ -0,0 +1,14 @@
+from readdata import a , b
+from addition import add
+from substraction import sub
+from division import div
+from multiplication import multiply
+def main():
+    print("Basic Calculator")
+    add(a,b)
+    sub(a,b)
+    div(a,b)
+    multiply(a,b)
+
+if __name__ == "__main__":
+    main()
